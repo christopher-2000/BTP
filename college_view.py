360b1fb42c47b9332ed99252a298ad114424dd19
christopher-2000 <christopher.geever@gmail.com>
2022-03-27 17:08:20 +0530

    search

--- college_view.py
+++ college_view.py
@@ -1,8 +1,11 @@
+from ast import excepthandler
+from turtle import color
 from pandas import read_csv,DataFrame
 import streamlit as st
 import time
 from operations import*
 from annotated_text import annotated_text
+from sess import SessionState
 
 data = read_csv("data/cbf_data.csv",encoding='ISO-8859-1')
 shiksha_data = read_csv("data/display_data.csv")
@@ -43,6 +46,22 @@ def display_college(college):
     
     
 def display_regionwise(region):
+    reg_colleges = data[data['Region']==region]
+    
+    for i,k,l in zip(reg_colleges['Institution Name'],reg_colleges['Rank in 2020'],reg_colleges['Overall Score']):
+        try:
+            link = shiksha_data[shiksha_data['college']==i]['link'].values[0]
+        except:
+            link = "#"
+        st.markdown(f"""
+        <div style="margin-bottom:5px;">
+        <h4>{i}</h4>
+        <h5>Rank in 2020: <span  style = "color:green;">{k}</span> ----  Overall Score: <span  style = "color:red;">{l}</span></h5>
+        ---
+        </div>
+        """,unsafe_allow_html=True)
+
+       
     pass
 
 
--- main_page.py
+++ main_page.py
@@ -1,3 +1,4 @@
+import re
 from pandas import read_csv
 import streamlit as st
 from college_view import*
@@ -8,12 +9,15 @@ def display_top():
     st.write("""
     ## Top 5 Universites according to QS world rankings 2020
     """)
-    for i in range(5):
-        st.markdown("{}. {} {}".format(i+1,data["Institution Name"][i],data["Overall Score"][i]))
+    for i in range(1,6):
+        st.markdown("{}. {} {}".format(i,data["Institution Name"][i],data["Overall Score"][i]))
 
 def search_college():
     college = st.selectbox('Search for a college',data["Institution Name"])
-    region = st.selectbox('Search for a region',data["Region"])
+    region = st.selectbox('Search for a region',data["Region"].unique())
     if st.button('Search now'):
-        display_college(college)
+        if college=='NONE' and region!='NONE':
+            display_regionwise(region)
+        else:
+            display_college(college)
 
